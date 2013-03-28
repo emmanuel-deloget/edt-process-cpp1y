7925b1bebb4950c05cfb2b3c0cb17eb7e2789868
Emmanuel Deloget <logout@free.fr>
2013-03-28 11:38:08 +0100

    ptest: adding an exec() check
    This check works on linux only. The best way to show off exec()
    should be to exec ourselves with a few parameters, something
    like ./ptest nochk PID, where PID is the PID of the child. The
    program would then test whether this_process::get_id() == PID.

--- src/ptest.cc
+++ src/ptest.cc
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <process>
 
-int main()
+int check_id()
 {
 	std::process p([=]() {
 		std::cout << "- this process    : " << std::this_process::get_id() << std::endl;
@@ -14,4 +14,20 @@ int main()
 	p.join();
 	std::cout << "+ after join()    : " << p.get_id() << std::endl;
 	std::cout << "+ this process    : " << std::this_process::get_id() << std::endl;
+}
+
+int check_exec()
+{
+	std::cout << "I'm the parent, pid=" << std::this_process::get_id() << std::endl;
+	std::process p([=]() {
+		std::cout << "I'm the child, pid=" << std::this_process::get_id() << std::endl;
+		std::this_process::exec("/bin/bash", "-c", "echo \"I'm the exec()uted child, pid=$$\"");
+	});
+	p.join();
+}
+
+int main()
+{
+	check_id();
+	check_exec();
 }
