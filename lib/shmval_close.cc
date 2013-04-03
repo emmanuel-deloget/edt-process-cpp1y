59aa6eff3cf7ad5d004f2150de474f7c32cc3354
Emmanuel Deloget <logout@free.fr>
2013-04-03 17:48:31 +0200

    named_mutex: remove an unneeded trace

--- lib/shmval_close.cc
+++ lib/shmval_close.cc
@@ -1,16 +1,13 @@
 #include <sys/mman.h>
 #include <unistd.h>
-#include <stdio.h>
 
 namespace std { namespace __bits {
 
 	void __shmval_unmap_and_close(const char *__n, bool __o, void *__p, size_t __s)
 	{
 		munmap(__p, __s);
-		if (__o) {
-			printf("   shm unlink     : %s\n", __n);
+		if (__o)
 			shm_unlink(__n);
-		}
 	}
 
 } }
